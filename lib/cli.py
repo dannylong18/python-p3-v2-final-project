2f808f4bbf0cb94be8a82e255b7f09041c11e0e2
Daniel Long <danielwlong618@gmail.com>
2024-04-01 22:13:48 -0500

    creating name property

--- lib/cli.py
+++ lib/cli.py
@@ -9,7 +9,7 @@ from helpers import (
 def main():
     while True:
         menu()
-        choice = input("> ")
+        choice = input(">> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
--- lib/models/treatment.py
+++ lib/models/treatment.py
@@ -5,7 +5,21 @@ class Treatment:
     def __init__(self, name, date_started, id=None):
         self.id = id
         self.name = name
-        self.dated_started = date_started
+        self.date_started = date_started
 
     def __repr__(self):
-        return f"<Treatment {self.id}: {self.name}, {self.dated_started}>"
+        return f"<Treatment {self.id}: {self.name}, {self.date_started}>"
+    
+    @property
+    def name (self):
+        return self._name 
+    
+    @name.setter
+    def name (self, name):
+        if isinstance (name, str) and len(name):
+            self._name = name
+        
+        else:
+            raise ValueError ('Name must be a non-empty string')
+        
+    
